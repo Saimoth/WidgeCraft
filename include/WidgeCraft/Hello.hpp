eddbeda42274bc7d9c1aae8ae811c79eae553cdf
Saimoth <wesleyt@hotmail.co.uk>
2025-08-12 20:13:31 +0100

    Create Hello.hpp

---
+++ include/WidgeCraft/Hello.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+
+namespace WidgeCraft {
+    struct Hello {
+        static void Print() {
+            std::cout << "Hello, WidgeCraft!\n";
+        }
+    };
+}
