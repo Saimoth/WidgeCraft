dfbba69da2b67f606a3235b9380dba4ea46a1b9e
Saimoth <wesleyt@hotmail.co.uk>
2026-07-10 05:59:42 +0100

    Integrate input and window lifecycle controls

--- include/WidgeCraft/Window.hpp
+++ include/WidgeCraft/Window.hpp
@@ -1,9 +1,10 @@
 #pragma once
+
 #include "WidgeCraft/Frame.hpp"
+#include "WidgeCraft/Input.hpp"
 
 #include <string>
 
-// Forward declaration of GLFW window type
 struct GLFWwindow;
 
 namespace WidgeCraft {
@@ -13,25 +14,44 @@ namespace WidgeCraft {
         Window(int width, int height, const std::string& title);
         ~Window();
 
-        bool shouldClose() const;
-        void pollEvents() const;
+        Window(const Window&) = delete;
+        Window& operator=(const Window&) = delete;
 
-        // Needed so we can swap buffers in main
+        bool shouldClose() const;
+        void requestClose();
+        void pollEvents();
+        void swapBuffers() const;
+
+        void setVSync(bool enabled);
+        bool isVSyncEnabled() const { return m_vsyncEnabled; }
+        void setTitle(const std::string& title);
+
         GLFWwindow* getNativeHandle() const { return m_window; }
         int getWidth() const { return m_width; }
         int getHeight() const { return m_height; }
+        float getAspectRatio() const;
+
+        Input& getInput() { return m_input; }
+        const Input& getInput() const { return m_input; }
 
         Frame& getRootFrame() { return m_rootFrame; }
         const Frame& getRootFrame() const { return m_rootFrame; }
 
     private:
-        GLFWwindow* m_window;
-        int m_width;
-        int m_height;
+        GLFWwindow* m_window = nullptr;
+        int m_width = 0;
+        int m_height = 0;
+        bool m_vsyncEnabled = true;
+        Input m_input;
         Frame m_rootFrame;
 
         void updateSize(int width, int height);
+
         static void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+        static void keyCallback(GLFWwindow* window, int key, int scanCode, int action, int modifiers);
+        static void mouseButtonCallback(GLFWwindow* window, int button, int action, int modifiers);
+        static void cursorPositionCallback(GLFWwindow* window, double x, double y);
+        static void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
     };
 
 } // namespace WidgeCraft
